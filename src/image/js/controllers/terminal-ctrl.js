d45e8e987fe34411eb1831243099d99553aebfa3
lovening <1433391@tongji.edu.cn>
2015-03-31 14:05:10 +0800

    update

--- src/image/js/controllers/terminal-ctrl.js
+++ src/image/js/controllers/terminal-ctrl.js
@@ -5,6 +5,8 @@ angular.module('Image')
     .controller('TerminalCtrl', ['$scope', '$cookieStore','$stateParams', TerminalCtrl]);
 
 function TerminalCtrl($scope,$cookieStore,$stateParams) {
-    $scope.apiUrl = "http://localhost:8080";
+    $scope.apiUrl = "http://localhost:8080/user/";
+    $scope.uid = 1;
     $scope.baseimage = $stateParams.base;
+
 }
