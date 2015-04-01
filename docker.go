efaff2bc0f974e3084f3dfec43b89d2897d7ca07
lovening <1433391@tongji.edu.cn>
2015-04-01 15:33:25 +0800

    push

--- docker.go
+++ docker.go
@@ -12,13 +12,13 @@ var (
 
 //func dockerCommit() {
 func main() {
-	img, err := client.CommitContainer(docker.CommitContainerOptions{Container: "ffc4dfc4827c"})
+	img, err := client.CommitContainer(docker.CommitContainerOptions{Container: "0383b55de49b", Repository: "hahaha"})
 	fmt.Println(img)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	err = client.RemoveContainer(docker.RemoveContainerOptions{ID: "ffc4dfc4827c"})
+	//	err = client.RemoveContainer(docker.RemoveContainerOptions{ID: "ffc4dfc4827c"})
 }
 
 func deleteImageAPI(id string) {
