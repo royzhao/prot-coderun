7dd91287e715f58dbbb14f32ecb9dd7e0430ae43
royzhao <524510356@qq.com>
2015-04-27 20:44:28 +0800

    修复terminal的bug

--- src/dashboard/js/module.js
+++ src/dashboard/js/module.js
@@ -1,3 +1,3 @@
 angular.module('RDash', ['ui.bootstrap', 'ui.router', 'ngCookies','ngStorage','permission','ngDialog','ngResource']);
 var baseUrl = "/api";
-var ssoUrl = "http://sso.learn4me.com/login?refer=";
+var ssoUrl = "http://sso.learn4me.com/login.php?refer=";
